e3d431bac9d0279fc56d6a131890bb9ddb5c3ec7
Darkxps <jvitorlopesalmendra@gmail.com>
2024-03-14 11:41:16 -0400

    a

--- back/Atividades/atv4/atvs.js
+++ back/Atividades/atv4/atvs.js
@@ -12,15 +12,26 @@ while(conta<=10){
 /* Soma notas*/
 
 
-let aluno = Ent.questionInt ('digite a quantia de alunos: ')
-let cont = 0
 
-while(cont<=aluno){
-    let nota1 = Ent.questionFloat("Digite a nota do 1° bimestre: ")
-    let nota2 = Ent.questionFloat("Digite a nota do 2° bimestre: ")
-    let nota3 = Ent.questionFloat("Digite a nota do 3° bimestre: ")
-    let nota4 = Ent.questionFloat("Digite a nota do 4° bimestre: ")
-    let soma = (nota1+nota2+nota3+nota4) /4
-    console.log(`sua nota é ${soma}`)
+
+const numAlunos = 4; 
+
+let somaTotal = 0;
+
+for (let i = 1; i <= numAlunos; i++) {
+    console.log(`Aluno ${i}:`);
+    let nota1 = Ent.questionFloat("Digite a nota do 1° bimestre: ");
+    let nota2 = Ent.questionFloat("Digite a nota do 2° bimestre: ");
+    let nota3 = Ent.questionFloat("Digite a nota do 3° bimestre: ");
+    let nota4 = Ent.questionFloat("Digite a nota do 4° bimestre: ");
+
+    let mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+    console.log(`A média do Aluno ${i} é ${mediaAluno.toFixed(2)}\n`);
+
+
+    somaTotal += mediaAluno;
 }
 
+
+let mediaTurma = somaTotal / numAlunos;
+console.log(`A média da turma é ${mediaTurma}`);
