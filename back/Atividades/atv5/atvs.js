baa2e3c6bfc7840d23ec63beae6646871bee8ced
Darkxps <jvitorlopesalmendra@gmail.com>
2024-03-28 11:44:23 -0400

    gav

---
+++ back/Atividades/atv5/atvs.js
@@ -0,0 +1,33 @@
+//import Ent from "readline-sync";
+
+//atv1
+
+function toUpperCase(str) {
+    return str.toUpperCase();
+  }
+  
+  const str = "se quiser sim meu mano";
+  const strUpperCase = toUpperCase(str);
+  
+  console.log(strUpperCase); 
+
+// atv2
+
+function verificarParImpar(numero) {
+    if (numero % 2 === 0) {
+      return "par";
+    } else {
+      return "ímpar";
+    }
+  }
+  
+  console.log(verificarParImpar(10)); 
+  console.log(verificarParImpar(15)); 
+
+  //atv3
+
+function somar(num1, num2){
+    return somar = num1+num2
+
+}
+    console.log(somar(1,2))
--- back/teste.js
+++ back/teste.js
@@ -1,31 +1,7 @@
+let valor_1 = 5;
 
-let nome = "João vitor Lopes"
-const idd = "16"
+const valor_2 = 3;
 
-console.log("Ola, meu nome é ", nome, "e tenho", idd, "Anos de idade");
+valor_1 += valor_2 ;
 
-
-
-let str ="Teste0"
-let maius = str.toUpperCase();
-let minus = str.toLowerCase();
-
-console.log("Frase original ", str) 
-console.log("Frase maiuscula ", maius) 
-console.log("Frase Minuscula ", minus) 
-
-
-let numero = Number(9);
-
-if (typeof numero === 'number') {
-    console.log("A variável numero é do tipo Number");
-} else {
-    console.log("A variável numero não é do tipo Number");
-}
-
-
-var peso = ("80")
-var Altura = ("187")
-
-console.log("Seu imc é", peso/(Altura*Altura))
-
+console.log(valor_1);
