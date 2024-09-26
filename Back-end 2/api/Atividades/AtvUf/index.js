ea6edc8ebdc9f1af788c0d002fa084c96f567ad7
Darkxps <jvitorlopesalmendra@gmail.com>
2024-09-26 10:59:28 -0400

    jah pd al-mossar?

--- Back-end 2/api/Atividades/AtvUf/index.js
+++ Back-end 2/api/Atividades/AtvUf/index.js
@@ -7,6 +7,12 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUf);
 });
 
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf); 
+    const uf = colecaoUf.colecaoUf.find(u => u.id ===idUF);
+    res.json(uf);
+});
+
 app.listen(8080, () => {
     console.log('Servidor iniciado na porta 8080');
 });
